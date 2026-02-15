c27a2920a971787c5625bc11150a4255b5245906
Rakesh M <rakesh123@gmail.com>
2026-02-15 23:44:53 +0530

    fixed render port issue

--- app.py
+++ app.py
@@ -35,5 +35,7 @@ init_routes(app)
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
